9c8e6de7f2341fa0c2642517d93c31bb4aa9256e
reza <raftx24@gmail.com>
2020-06-30 20:09:52 +0430

    moves database to root

--- database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
+++ database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
@@ -4,7 +4,7 @@ use LaravelEnso\Migrator\Database\Migration;
 
 class CreateStructureForControlPanel extends Migration
 {
-    protected $permissions = [
+    protected array $permissions = [
         ['name' => 'api.controlPanelApi.statistics', 'description' => 'Get statistics', 'is_default' => false],
         ['name' => 'api.controlPanelApi.actions', 'description' => 'Get available Actions', 'is_default' => false],
         ['name' => 'api.controlPanelApi.action', 'description' => 'Do an action', 'is_default' => false],
