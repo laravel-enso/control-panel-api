1f1e8a4ae64bd8999a94eecabc5ca7265249902a
reza <raftx24@gmail.com>
2020-07-21 15:19:48 +0430

    renames routes - needs to update ControlPanelApiSeeder with `php artisan vendor:publish --tag=control-panel-api-seeder --force`

--- database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
+++ database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
@@ -5,8 +5,8 @@ use LaravelEnso\Migrator\Database\Migration;
 class CreateStructureForControlPanel extends Migration
 {
     protected array $permissions = [
-        ['name' => 'api.controlPanelApi.statistics', 'description' => 'Get statistics', 'is_default' => false],
-        ['name' => 'api.controlPanelApi.actions', 'description' => 'Get available Actions', 'is_default' => false],
-        ['name' => 'api.controlPanelApi.action', 'description' => 'Do an action', 'is_default' => false],
+        ['name' => 'apis.controlPanel.statistics', 'description' => 'Get statistics', 'is_default' => false],
+        ['name' => 'apis.controlPanel.actions', 'description' => 'Get available Actions', 'is_default' => false],
+        ['name' => 'apis.controlPanel.action', 'description' => 'Do an action', 'is_default' => false],
     ];
 }
