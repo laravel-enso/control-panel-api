a6439d92f296a6886d6c5c114eee57156bc73d12
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 15:47:41 +0300

    updates syntax

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\ControlPanelApi\Http\Controllers\Statistics;
+use LaravelEnso\ControlPanelApi\Http\Controllers\Actions;
+use LaravelEnso\ControlPanelApi\Http\Controllers\Action;
+use LaravelEnso\ControlPanelApi\Http\Controllers\DownloadLog;
 
-Route::namespace('LaravelEnso\ControlPanelApi\Http\Controllers')
-    ->name('apis.controlPanel.')
+Route::name('apis.controlPanel.')
     ->prefix('apis/controlPanel')
     ->group(function () {
         Route::middleware(['api', 'auth:sanctum', 'core-api'])
             ->group(function () {
-                Route::get('statistics', 'Statistics')->name('statistics');
-                Route::get('actions', 'Actions')->name('actions');
-                Route::any('{action}', 'Action')->name('action');
+                Route::get('statistics', Statistics::class)->name('statistics');
+                Route::get('actions', Actions::class)->name('actions');
+                Route::any('{action}', Action::class)->name('action');
             });
 
         Route::middleware(['signed', 'bindings'])
             ->prefix('action')
             ->as('action.')
-            ->group(fn () => Route::get('downloadLog', 'DownloadLog')->name('downloadLog'));
+            ->group(fn () => Route::get('downloadLog', DownloadLog::class)->name('downloadLog'));
     });
