b63d8f30527abe879e6ed7e0e7e73939354ce3bf
Mihai <mihai.ocneanu@gmail.com>
2022-03-02 13:25:44 +0200

    updated migrations

--- database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
+++ database/migrations/2020_06_22_118000_create_structure_for_control_panel.php
@@ -2,11 +2,11 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-class CreateStructureForControlPanel extends Migration
+return new class extends Migration
 {
     protected array $permissions = [
         ['name' => 'apis.controlPanel.statistics', 'description' => 'Get statistics', 'is_default' => false],
         ['name' => 'apis.controlPanel.actions', 'description' => 'Get available Actions', 'is_default' => false],
         ['name' => 'apis.controlPanel.action', 'description' => 'Do an action', 'is_default' => false],
     ];
-}
+};
